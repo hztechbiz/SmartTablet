21bc53eccf84544bde06628c6e2c449a4224c85e
faizan <faizan.fiverr07@gmail.com>
2018-04-17 02:12:53 +0500

    checking

---
+++ app/src/main/java/com/smartapp/hztech/smarttebletapp/topBarActivity.java
@@ -0,0 +1,13 @@
+package com.smartapp.hztech.smarttebletapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class topBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_bar);
+    }
+}
